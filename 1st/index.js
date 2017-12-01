af61961d775130f3668fd3b50700efff98586cf2
Alexander Lichter <manniL@gmx.net>
2017-12-01 22:50:42 +0100

    Solve second task for first day

--- 1st/index.js
+++ 1st/index.js
@@ -2,7 +2,7 @@ const fs = require('fs-extra')
 const path = require('path')
 const Lazy = require('lazy.js')
 
-const solveCaptcha = (numberString) => {
+const solveFirst = (numberString) => {
   const splitNumbers = numberString.split('')
 
   //Push first number again for evaluation
@@ -16,4 +16,31 @@ const solveCaptcha = (numberString) => {
   console.log(result)
 }
 
-fs.readFile(path.join(__dirname, 'input.txt'), 'utf8').then(solveCaptcha)
+const solveSecondCaptcha = (numberString) => {
+  const splitNumbers = numberString.split('')
+
+  const sumUpIfEqual = (carry, pair) => {
+    return carry + (pair[0] === pair[1] ? pair[0] : 0)
+  }
+
+  let sequence = Lazy(splitNumbers)
+    .map((o) => parseInt(o))
+
+  let result = sequence.map((value, index) => {
+    let halfLength = sequence.length() / 2
+    if (index <= halfLength) {
+      return [value, sequence.get(halfLength + index)]
+    }
+    return undefined
+  })
+    .filter()
+    .reduce(sumUpIfEqual, 0) * 2
+  console.log(result)
+}
+
+const solveCaptchas = (numberString) => {
+  solveFirst(numberString)
+  solveSecondCaptcha(numberString)
+}
+
+fs.readFile(path.join(__dirname, 'input.txt'), 'utf8').then(solveCaptchas)
