f2561356ddc03234e00dcd171872b7e80c8b6478
Alexander Lichter <manniL@gmx.net>
2017-12-03 19:26:45 +0100

    Solve day two task two

--- 2nd/index.js
+++ 2nd/index.js
@@ -7,8 +7,20 @@ const mapDifference = (arr) => {
   return seq.max() - seq.min()
 }
 
+const mapEvenMod = (arr) => {
+  let seq = Lazy(arr).map(i => parseInt(i)).sort()
+  return seq.map((number) => {
+    return seq.find((sn) => sn !== number && sn % number === 0) / number
+  }).filter().first()
+}
+
 const solveFirstChecksum = (seq) => {
   const result = seq.map(mapDifference).reduce((carry, integer) => carry + integer, 0)
+  console.log(result)
+}
+
+const solveSecondChecksum = (seq) => {
+  const result = seq.map(mapEvenMod).reduce((carry, integer) => carry + integer, 0)
   console.log(result)
 }
 
@@ -16,6 +28,7 @@ const solveChecksums = (numberString) => {
   const seq = Lazy(numberString.split('\n')).map((line) => line.split('\t'))
 
   solveFirstChecksum(seq)
+  solveSecondChecksum(seq)
 }
 
 fs.readFile(path.join(__dirname, 'input.txt'), 'utf8').then(solveChecksums)
