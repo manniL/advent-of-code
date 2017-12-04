7d56a899d00f4d8e196574f1fad14285ef1ff5e3
Alexander Lichter <manniL@gmx.net>
2017-12-04 09:48:14 +0100

    Solve day 4 task two

--- 4th/index.js
+++ 4th/index.js
@@ -6,10 +6,15 @@ const solveFirstPassphrase = (seq) => {
   return seq.map((lineSeq) => lineSeq.uniq().toArray().length === lineSeq.length()).filter().toArray().length
 }
 
+const mapAnagram = (lineSeq) => lineSeq.map((word) => word.split('').sort('').join('')).uniq().toArray().length === lineSeq.length()
+
+const solveSecondPassphrase = (seq) => {
+  return seq.map(mapAnagram).filter().toArray().length
+}
 const solvePassphrases = (numberString) => {
   const seq = Lazy(numberString.split('\n')).map((line) => Lazy(line.split(' ')))
 
-  console.log(solveFirstPassphrase(seq))
+  console.log(solveFirstPassphrase(seq), solveSecondPassphrase(seq))
 }
 
 fs.readFile(path.join(__dirname, 'input.txt'), 'utf8').then(solvePassphrases)
