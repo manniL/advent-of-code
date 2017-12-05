644afe30ef63a0e6226f1a5447c80177ba09f47f
Alexander Lichter <manniL@gmx.net>
2017-12-05 14:24:41 +0100

    Solve day 5 task 2

--- 5th/index.js
+++ 5th/index.js
@@ -1,6 +1,5 @@
 const fs = require('fs-extra')
 const path = require('path')
-const Lazy = require('lazy.js')
 
 const solveFirstInstruction = (seq) => {
   let nextPos = 0
@@ -13,9 +12,21 @@ const solveFirstInstruction = (seq) => {
   return steps
 }
 
+const solveSecondInstruction = (seq) => {
+  let nextPos = 0
+  let steps = 0
+
+  while (nextPos >= 0 && nextPos < seq.length) {
+    nextPos = nextPos + ((seq[nextPos] >= 3) ? seq[nextPos]-- : seq[nextPos]++)
+    steps++
+  }
+
+  return steps
+}
+
 const solveInstructions = (numberString) => {
   const seq = numberString.split('\n').map((n) => parseInt(n))
-  console.log(solveFirstInstruction(seq))
+  console.log(solveFirstInstruction([...seq]), solveSecondInstruction([...seq]))
 }
 
 fs.readFile(path.join(__dirname, 'input.txt'), 'utf8').then(solveInstructions)
