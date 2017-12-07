371b702a32e6f51538b5a9a2a6163b728b9c6718
Alexander Lichter <manniL@gmx.net>
2017-12-07 23:28:38 +0100

    Solve day 7 task 2
    + That one was quite hard!
    + It might take a little to compute

--- 7th/index.js
+++ 7th/index.js
@@ -21,6 +21,60 @@ const solveFirstTask = (seq) => {
   return seq.map(p => p.name).without(seq.map((p) => p.programs).filter().flatten().uniq()).first()
 }
 
+const solveSecondTask = (seq, name) => {
+
+  const nodeWeight = (seq, name) => {
+
+    let node = seq.findWhere({name})
+
+    if (node.programs !== false) {
+      return node.programs.reduce((c, p) => c + nodeWeight(seq, p), 0) + node.weight
+    }
+    return node.weight
+  }
+
+  const childWeight = (seq, name) => {
+    let node = seq.findWhere({name})
+
+    return node.programs === false ? 0 : node.programs.reduce((c, p) => c + nodeWeight(seq, p), 0)
+  }
+  console.log('init mapping')
+
+  let newSeq = seq.map((p) => ({
+    ...p,
+    childWeight: childWeight(seq, p.name),
+    absWeight: childWeight(seq, p.name) + p.weight
+  }))
+
+  let lastNode = null
+  let currentNode = newSeq.findWhere({name})
+
+  console.log('Let\'s get ready to rumble!')
+  while (true) {
+    console.log('Start now with', lastNode, currentNode)
+
+    if (!currentNode.programs) {
+      break
+    }
+    let programs = currentNode.programs.map((name) => newSeq.findWhere({name}))
+    let uniqueWeights = programs.map((node) => node.childWeight + node.weight).filter((weight, i, self) => {
+      return self.lastIndexOf(weight) === self.indexOf(weight)
+    })
+
+    if (!uniqueWeights.length) {
+      break
+    }
+
+    lastNode = currentNode
+    currentNode = Lazy(programs).findWhere({absWeight: uniqueWeights[0]})
+  }
+  let evenWeight = lastNode.programs.map((name) => newSeq.findWhere({name}).absWeight).filter((a, i, self) => {
+    return self.lastIndexOf(a) !== i
+  })
+
+  return evenWeight - currentNode.childWeight
+}
+
 const solveTasks = (seq) => {
 
   let overview = Lazy(seq).split('\n').map((line) => {
@@ -37,8 +91,9 @@ const solveTasks = (seq) => {
       programs
     }
   })
+  let top = solveFirstTask(overview)
 
-  console.log(solveFirstTask(overview))
+  console.log(top, solveSecondTask(overview, top))
 
 }
 
