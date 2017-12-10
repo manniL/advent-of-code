e9b6acf7c4569e10848b45b09d898a0ef4f72c54
Alexander Lichter <manniL@gmx.net>
2017-12-10 19:10:57 +0100

    Solve day 10 task 2

--- 10/index.js
+++ 10/index.js
@@ -39,12 +39,45 @@ const solveFirstTask = (listSeq, lengths) => {
   return finalArray[0] * finalArray[1]
 
 }
+
+const solveSecondTask = (listSeq, asciiCodes) => {
+
+  let additionalCodes = [17, 31, 73, 47, 23]
+  additionalCodes.forEach((a) => asciiCodes.push(a))
+
+  let position = 0
+  let skipLen = 0
+  for (let round = 0; round < 64; round++) {
+    let asciiClone = [...asciiCodes]
+    while (asciiClone.length) {
+      let currentLength = asciiClone.shift()
+      let rotationOffset = (position + currentLength) % listSeq.length()
+      let rotatedSeq = listSeq.rotate(rotationOffset)
+      let reversedSeq = Lazy(rotatedSeq.toArray()).reverse().toArray().splice(0, currentLength)
+      let splitSeq = rotatedSeq.toArray().splice(0, rotatedSeq.toArray().length - currentLength)
+      reversedSeq.forEach((a) => splitSeq.push(a))
+      let backRotateSeq = Lazy(splitSeq).rotate(rotationOffset, true)
+      listSeq = Lazy(backRotateSeq)
+      position = (position + currentLength + skipLen) % listSeq.toArray().length
+      skipLen++
+    }
+  }
+
+  //XOR
+  let sparseHash = listSeq.toArray()
+  console.log(sparseHash)
+  let denseHash = []
+  while (sparseHash.length) {
+    denseHash.push(sparseHash.splice(0, 16).reduce((c, a) => c ^ a, 0))
+  }
+  return denseHash.map((a) => a.toString(16).length === 2 ? a.toString(16) : '0' + a.toString(16)).join('')
+}
 const solveTasks = (inputString) => {
   let list = Lazy(Lazy.range(256).toArray())
   let lengths = inputString.split(',')
+  let asciiInput = inputString.split('').map((char) => char.charCodeAt(0))
 
-  console.log(solveFirstTask(list, lengths))
+  console.log(solveFirstTask(list, lengths), solveSecondTask(list, asciiInput))
 
 }
-
 fs.readFile(path.join(__dirname, 'input.txt'), 'utf8').then(solveTasks)
