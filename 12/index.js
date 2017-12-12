a2abca6d633d4631d95237cc613370b138cb0fec
Alexander Lichter <manniL@gmx.net>
2017-12-12 13:10:04 +0100

    Solve day 12 task 2

--- 12/index.js
+++ 12/index.js
@@ -2,29 +2,36 @@ const fs = require('fs-extra')
 const path = require('path')
 const Lazy = require('lazy.js')
 
+const getPrograms = (seq, currentArray, parent) => {
+  currentArray.push(parent)
+  let parentNode = seq.findWhere({parent})
+  parentNode.programs.forEach((p) => !currentArray.includes(p) ? getPrograms(seq, currentArray, p) : null)
+
+  return currentArray
+}
+
 const solveFirstTask = (lines) => {
 
-  const getPrograms = (seq, currentArray, parent) => {
-    currentArray.push(parent)
-    let parentNode = seq.findWhere({parent})
-    parentNode.programs.forEach((p) => !currentArray.includes(p) ? getPrograms(seq, currentArray, p) : null)
-
-    return currentArray
-  }
-
-  let mappedLines = lines.map((line) => {
-    let [parent, , ...programs] = [...(line.split(' '))]
-    return {parent, programs}
-  })
-
-  let resultPrograms = getPrograms(Lazy(mappedLines), [], '0')
+  let resultPrograms = getPrograms(Lazy(lines), [], '0')
   return resultPrograms.reduce((c) => ++c, 0)
+}
 
+const solveSecondTask = (lines) => {
+  let groups = 0
+  while (lines.length) {
+    let resultPrograms = getPrograms(Lazy(lines), [], lines[0].parent)
+    lines = lines.filter((node) => !resultPrograms.includes(node.parent))
+    groups++
+  }
+  return groups
 }
 
 const solveTasks = (inputString) => {
-  let lines = inputString.replace(/,/g, '').split('\n')
-  console.log(solveFirstTask([...lines]))
+  let lines = inputString.replace(/,/g, '').split('\n').map((line) => {
+    let [parent, , ...programs] = [...(line.split(' '))]
+    return {parent, programs}
+  })
+  console.log(solveFirstTask([...lines]), solveSecondTask([...lines]))
 
 }
 fs.readFile(path.join(__dirname, 'input.txt'), 'utf8').then(solveTasks)
