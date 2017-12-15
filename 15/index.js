888802ffae90996847154f10788e272ac5120a72
Alexander Lichter <manniL@gmx.net>
2017-12-15 14:05:03 +0100

    Solve day 15 task 2

--- 15/index.js
+++ 15/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs-extra')
 const path = require('path')
 
-const solveFirstTask = (generators) => {
+const solveBothTasks = (generators, partTwo = false) => {
   let divisor = 2147483647
   let judgeCount = 0
   let twoPairsCreated = 0
@@ -10,14 +10,22 @@ const solveFirstTask = (generators) => {
     g.value = (g.value * g.factor) % divisor
     return g
   }
+  const mapGenWithMod = (g) => {
+    do {
+      g.value = (g.value * g.factor) % divisor
+    } while (g.value & g.mod)
+    return g
+  }
 
-  while (twoPairsCreated < 4 * 10 ** 7) {
+  let mapFn = partTwo ? mapGenWithMod : mapGen
+  let maxPairs = partTwo ? 5 * 10 ** 6 : 4 * 10 ** 7
+  while (twoPairsCreated < maxPairs) {
 
     if ((generators[0].value & 0xFFFF) === (generators[1].value & 0xFFFF)) {
       judgeCount++
     }
 
-    generators = generators.map(mapGen)
+    generators = generators.map(mapFn)
     twoPairsCreated++
   }
   return judgeCount
@@ -27,9 +35,9 @@ const solveTasks = (startValues) => {
   startValues = startValues.split('\n').map((s) => parseInt(/(\d+)/gim.exec(s)[1]))
 
   let generators = [
-    {value: startValues[0], factor: 16807},
-    {value: startValues[1], factor: 48271}
+    {value: startValues[0], factor: 16807, mod: 3},
+    {value: startValues[1], factor: 48271, mod: 7}
   ]
-  console.log(solveFirstTask(generators))
+  console.log(solveBothTasks(generators), solveBothTasks(generators, true))
 }
 fs.readFile(path.join(__dirname, 'input.txt'), 'utf8').then(solveTasks)
