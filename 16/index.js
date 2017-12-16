c4a70b45a6f025e7ae25eaf137fd6283b456ac7e
Alexander Lichter <manniL@gmx.net>
2017-12-16 16:47:56 +0100

    Solve day 16 task 2

--- 16/index.js
+++ 16/index.js
@@ -1,11 +1,8 @@
 const fs = require('fs-extra')
 const path = require('path')
 
-const solveFirstTask = (instructions) => {
-  let programs = [...'abcdefghijklmnop']
-
+const dance = (programs, instructions) => {
   instructions.forEach((i) => {
-    console.log(programs.join(''))
     let firstLetter = i[0]
     let swapIndices, swapLetters
     switch (firstLetter) {
@@ -30,11 +27,29 @@ const solveFirstTask = (instructions) => {
         break
     }
   })
-  return programs.join('')
+  return programs
+}
+
+const solveFirstTask = (instructions) => {
+  return dance([...'abcdefghijklmnop'], instructions).join('')
+}
+
+const solveSecondTask = (instructions) => {
+  let finalPosition = [...'abcdefghijklmnop']
+  let positions = [finalPosition.join('')]
+  for (let count = 0; count < 10 ** 9; count++) {
+    finalPosition = dance(finalPosition, instructions)
+    if (!positions.includes(finalPosition.join(''))) {
+      positions.push(finalPosition.join(''))
+    } else {
+      break
+    }
+  }
+  return positions[(10 ** 9 % positions.length)]
 }
 
 const solveTasks = (input) => {
   input = input.split(',')
-  console.log(solveFirstTask(input))
+  console.log(solveFirstTask(input), solveSecondTask(input))
 }
 fs.readFile(path.join(__dirname, 'input.txt'), 'utf8').then(solveTasks).catch(e => console.error(e))
