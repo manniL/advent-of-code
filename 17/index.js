56a763abfc5dad8da5d6372944c15243df250dae
Alexander Lichter <manniL@gmx.net>
2017-12-17 10:56:59 +0100

    Solve day 17 task 2

--- 17/index.js
+++ 17/index.js
@@ -2,7 +2,6 @@ const fs = require('fs-extra')
 const path = require('path')
 
 const solveFirstTask = (input) => {
-
   let spinlock = [0]
   let position = 0
   for (let count = 1; count <= 2017; count++) {
@@ -13,7 +12,20 @@ const solveFirstTask = (input) => {
   return spinlock[(position + 1) % spinlock.length]
 }
 
+const solveSecondTask = (input) => {
+  let position = 0
+  let result = 1
+  for (let count = 0; count <= 5E7; count++) {
+    position = (position + input + 1) % (count + 1)
+    if (position === 0) {
+      result = count + 1
+    }
+  }
+  return result
+}
+
 const solveTasks = (input) => {
-  console.log(solveFirstTask(parseInt(input)))
+  input = parseInt(input)
+  console.log(solveFirstTask(input), solveSecondTask(input))
 }
 fs.readFile(path.join(__dirname, 'input.txt'), 'utf8').then(solveTasks).catch(e => console.error(e))
