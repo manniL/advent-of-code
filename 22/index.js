b1468eac8f71062895f1ac160c1c9fb21311170b
Alexander Lichter <manniL@gmx.net>
2017-12-23 01:45:01 +0100

    Solve day 22 task 2

--- 22/index.js
+++ 22/index.js
@@ -1,6 +1,8 @@
 const fs = require('fs-extra')
 const path = require('path')
 
+const STATES = ['.', 'W', '#', 'F']
+
 const DIRECTIONS = {
   TOP: [-1, 0],
   RIGHT: [0, 1],
@@ -13,13 +15,13 @@ const mod = (int, n) => {
 }
 
 class Map {
-  constructor (plan) {
+  constructor (plan, expandSize = 3E3, partTwo = false) {
+    this.partTwo = partTwo
     this.plan = plan
     this.currentPosition = [0, 0]
-    this.expandPlan(3E3)
+    this.expandPlan(expandSize)
     this.currentPosition = this.determineMiddle()
     this.facing = 0
-    this.burstCount = 0
     this.infectCount = 0
   }
 
@@ -38,35 +40,57 @@ class Map {
       a.push(...'.'.repeat(size))
       return a
     })
-
-    this.currentPosition = this.currentPosition.map((i) => i + size)
   }
 
   step () {
-    let infectedStatus = this.isInfected()
-    if (!infectedStatus) {
+    if (this.willNodeBeInfected()) {
       this.infectCount++
     }
-    this.burstCount++
 
-    this.changeNode(!infectedStatus)
-    this.turn(infectedStatus)
+    this.changeNode()
+    this.turn()
     this.moveForward()
   }
 
-  isInfected () {
-    let [x, y] = this.currentPosition
-    return this.plan[x][y] === '#'
+  willNodeBeInfected () {
+    return this.currentStateNumber() === (this.partTwo ? 1 : 0)
   }
 
-  changeNode (infect = true) {
-    let [x, y] = this.currentPosition
-    this.plan[x][y] = (infect) ? '#' : '.'
+  currentStateNumber () {
+    let currentState = this.currentState()
+    return STATES.reduce((f, v, i) => v === currentState ? i : f, 0)
   }
 
-  turn (right = true) {
+  currentState () {
+    let [x, y] = this.currentPosition
+    return this.plan[x][y]
+  }
+
+  changeNode () {
+    let [x, y] = this.currentPosition
+    let partOneNewState = this.currentState() === '#' ? '.' : '#'
+    let partTwoNewState = STATES[(this.currentStateNumber() + 1) % STATES.length]
+    this.plan[x][y] = (this.partTwo) ? partTwoNewState : partOneNewState
+  }
+
+  turn () {
     let dirLen = Object.keys(DIRECTIONS).length
-    this.facing = mod(this.facing + (right ? 1 : -1), dirLen)
+    let facingOffset = 0
+    if (this.partTwo) {
+      switch (this.currentState()) {
+        case 'W':
+          facingOffset = -1
+          break
+        case 'F':
+          facingOffset = 1
+          break
+        case '.':
+          facingOffset = 2
+      }
+    } else {
+      facingOffset = this.currentState() === '#' ? -1 : 1
+    }
+    this.facing = mod(this.facing + facingOffset, dirLen)
   }
 
   moveForward () {
@@ -84,9 +108,19 @@ const solveFirstTask = (plan) => {
   return map.infectCount
 }
 
+const solveSecondTask = (plan) => {
+
+  let map = new Map(plan, 3E3, true)
+
+  for (let i = 0; i < 1E7; i++) {
+    map.step()
+  }
+  return map.infectCount
+}
+
 const solveTasks = (plan) => {
   plan = plan.split('\n').map((i) => [...i])
-  console.log(solveFirstTask(plan))
+  console.log(solveFirstTask(plan), solveSecondTask(plan))
 }
 
 fs.readFile(path.join(__dirname, 'input.txt'), 'utf8').then(solveTasks)//.catch(e => console.error(e))
