0b7be2dec18dbb0622904912ae4452a7a69e34b7
Alexander Lichter <manniL@gmx.net>
2018-12-03 11:45:04 +0000

    Solve Day 1 Part 2

--- 01/index.js
+++ 01/index.js
@@ -4,9 +4,28 @@ const path = require('path')
 
 const run = async () => {
   const input = await readFile(path.join(__dirname, './input.txt'), 'utf-8')
-  const result = R.pipe(R.split('\n'), R.map(Number), R.sum)(input)
-  console.log('Part 1: ', result)
+  const formattedInput = formatInput(input)
+
+  console.log('Part 1:', partOne(formattedInput))
+  console.log('Part 2:', partTwo(formattedInput))
 }
+
+const partOne = R.sum
+// Not very satisfied as I'd love to distinguish between the halting and the normal output
+const partTwo = input => R.reduceWhile(
+  acc => Array.isArray(acc),
+  ([currentFrequency, frequencies], x) => {
+    const newFrequency = currentFrequency + x
+    if (frequencies.includes(newFrequency)) {
+      return newFrequency
+    }
+
+    return [newFrequency, frequencies.concat(newFrequency)]
+  },
+  [0, []],
+  // Hacky solution as you can't "cycle" the input of reduceWhile
+)(R.flatten(R.repeat(input, 150)))
 
 run()
 
+const formatInput = R.pipe(R.split('\n'), R.dropLast(1), R.map(R.pipe(R.trim, Number)))
