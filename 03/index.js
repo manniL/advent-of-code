db3eceb0ca8149e94e4e8737fb90d8cbcd6d5ac0
Alexander Lichter <manniL@gmx.net>
2018-12-03 20:59:41 +0000

    Solve Day 3 Part 2

--- 03/index.js
+++ 03/index.js
@@ -1,5 +1,5 @@
 const R = require('ramda')
-const {readFile} = require('../utils/fs.js')
+const { readFile } = require('../utils/fs.js')
 const path = require('path')
 
 const run = async () => {
@@ -7,7 +7,7 @@ const run = async () => {
   const formattedInput = formatInput(input)
 
   console.log('Part 1:', partOne(formattedInput))
-  // console.log('Part 2:', partTwo(formattedInput))
+  console.log('Part 2:', partTwo(formattedInput))
 }
 
 run()
@@ -28,28 +28,60 @@ const formatInput = R.pipe(R.split('\n'), R.dropLast(1), R.map(str => {
 }))
 
 const partOne = input => {
-  const inches = R.reduce(getInchesForFabric, {current: new Map(), dupes: new Map()}, input)
+  const inches = R.reduce(getInchesForFabric, { current: new Map(), dupes: new Map() }, input)
   // count of all elements - count of unique elements = count of duplicate items
   return inches.dupes.size
 }
 
-const getInchesForFabric = (({current, dupes}, {id, position, size}) => {
+const partTwo = input => {
+  const ids = R.pluck('id', input)
+  const inches = R.reduce(getInchesWithDupesForFabric, { current: new Map(), dupes: new Map() }, input)
+  // count of all elements - count of unique elements = count of duplicate items
+  const seenIds = new Set()
+  const addToSeen = a => a.forEach(v => seenIds.add(v))
+  inches.dupes.forEach(addToSeen)
+  return R.difference(ids, Array.from(seenIds))[0]
+}
+
+const getInchesWithDupesForFabric = (({ current, dupes }, { id, position, size }) => {
   // "Zero based"
   const rangeX = R.range(position.x + 1, position.x + size.x + 1)
   const rangeY = R.range(position.y + 1, position.y + size.y + 1)
-  const pairs = R.map(([x, y]) => `${x}_${y}`, R.xprod(rangeY, rangeX))
+  const pairs = R.map(([x, y]) => `${ x }_${ y }`, R.xprod(rangeY, rangeX))
   pairs.forEach(pair => {
     if (dupes.has(pair)) {
-      // In dupes and in current
+      dupes.set(pair, dupes.get(pair).concat(id))
       return
     }
     if (!current.has(pair)) {
       // Not in current at all
-      current.set(pair, 0)
+      current.set(pair, [id])
       return
     }
     // In current but not in dupes
-    dupes.set(pair, 0)
+    dupes.set(pair, [id, ...current.get(pair)])
   })
-  return {current, dupes}
+  return { current, dupes }
+})
+
+
+const getInchesForFabric = (({ current, dupes }, { id, position, size }) => {
+  // "Zero based"
+  const rangeX = R.range(position.x + 1, position.x + size.x + 1)
+  const rangeY = R.range(position.y + 1, position.y + size.y + 1)
+  const pairs = R.map(([x, y]) => `${ x }_${ y }`, R.xprod(rangeY, rangeX))
+  pairs.forEach(pair => {
+    if (dupes.has(pair)) {
+      dupes.set(pair, dupes.get(pair).concat(id))
+      return
+    }
+    if (!current.has(pair)) {
+      // Not in current at all
+      current.set(pair, [id])
+      return
+    }
+    // In current but not in dupes
+    dupes.set(pair, [id])
+  })
+  return { current, dupes }
 })
