f94b4e1459d0db7ac3201874e35140d1bd9692ba
Alexander Lichter <manniL@gmx.net>
2018-12-04 16:50:40 +0000

    Improve Day 1 Solution

--- 01/index.js
+++ 01/index.js
@@ -2,30 +2,44 @@ const R = require('ramda')
 const {readFile} = require('../utils/fs.js')
 const path = require('path')
 
-const run = async () => {
-  const input = await readFile(path.join(__dirname, './input.txt'), 'utf-8')
-  const formattedInput = formatInput(input)
-
-  console.log('Part 1:', partOne(formattedInput))
-  console.log('Part 2:', partTwo(formattedInput))
-}
-
 const partOne = R.sum
 // Not very satisfied as I'd love to distinguish between the halting and the normal output
-const partTwo = input => R.reduceWhile(
-  acc => Array.isArray(acc),
-  ([currentFrequency, frequencies], x) => {
-    const newFrequency = currentFrequency + x
-    if (frequencies.includes(newFrequency)) {
-      return newFrequency
-    }
 
-    return [newFrequency, frequencies.concat(newFrequency)]
-  },
-  [0, []],
-  // Hacky solution as you can't "cycle" the input of reduceWhile
-)(R.flatten(R.repeat(input, 150)))
+const partTwo = input => {
+  /* Courtesy to @rakeshpai for the idea. Way better than the old hacky solution */
+
+  // Impure infinite generator
+  const getNextInput = ((inputArray) => {
+    const inputGenerator = (function * () {
+      let currentIndex = 0
+
+      while (true) {
+        yield inputArray[currentIndex]
+        currentIndex = (currentIndex + 1) % inputArray.length
+      }
+    })()
+
+    return () => inputGenerator.next().value
+  })(input)
+  const init = () => [getNextInput(), new Set()]
+
+  return R.pipe(
+    init,
+    R.until(
+      ([currentFrequency, frequencies]) => frequencies.has(currentFrequency),
+      ([currentFrequency, frequencies]) => {
+        frequencies.add(currentFrequency)
+        currentFrequency += getNextInput()
+        return [currentFrequency, frequencies]
+      }),
+    R.head
+  )()
+}
 
 run()
 
-const formatInput = R.pipe(R.split('\n'), R.dropLast(1), R.map(R.pipe(R.trim, Number)))
+const formatInput = R.map(Number)
+const formattedInput = formatInput(input)
+
+console.log('Part 1:', partOne(formattedInput))
+console.log('Part 2:', partTwo(formattedInput))
