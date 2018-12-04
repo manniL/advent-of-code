5ab996e4e264a5b2bbe97aaf30997a8c88987613
Alexander Lichter <manniL@gmx.net>
2018-12-04 16:21:01 +0000

    Improve Day 2 Solution

--- 02/index.js
+++ 02/index.js
@@ -14,46 +14,45 @@ run()
 
 const formatInput = R.pipe(R.split('\n'), R.dropLast(1))
 
-const partOne = input => {
-  const {twoLetters, threeLetters} = R.applySpec({
-    twoLetters: getLetters(2),
-    threeLetters: getLetters(3)
-  })(input)
+const arrayPairIsEqual = R.converge(R.equals, [R.head, R.last])
 
-  return {threeLetters, twoLetters, result: threeLetters * twoLetters}
-}
+const valuesInclude = R.useWith(R.includes, [R.identity, R.values])
+const countByLetters = R.map(R.countBy(R.identity))
+const filterAndCount = R.pipe(R.filter, R.length)
 
-const partTwo = input => {
-  const pairs = distinctPairs(input, input)
-  const correctPairs = R.filter(([a, b]) => {
-    if (a === b) {
-      //ignore duplicates
-      return false
-    }
-    const zippedPairs = R.zip(R.split('', a), R.split('', b))
-    const equalZippedPairs = R.filter(arrayPairIsEqual, zippedPairs)
+const getLetters = R.useWith(filterAndCount, [valuesInclude, countByLetters])
 
-    // One letter mismatch
-    return equalZippedPairs.length === a.length - 1
-  }, pairs)
-
-  // Dedupe as xprod gives [1,2] and [2,1] as combinations (all possible ones, with "duplicates")
-  const correctPair = R.last(correctPairs)
-
-  return R.pipe(R.filter(arrayPairIsEqual), R.map(R.last), R.join(''))(R.zip(...correctPair))
-}
-
-const distinctPairs = (a, b) => {
+const createHalfTable = (a, b = [...a]) => {
   const [x, ...xs] = a
   const [, ...ys] = b
   return a.length && b.length
-    ? ys.map(y => [x, y]).concat(distinctPairs(xs, ys))
+    ? ys.map(y => [x, y]).concat(createHalfTable(xs, ys))
     : []
 }
 
-const arrayPairIsEqual = ([x, y]) => R.equals(x, y)
+const splitIntoCharacters = R.split('')
 
-const valuesInclude = R.useWith(R.includes, [R.identity, R.values])
-const countedByLetters = R.map(R.countBy(R.identity))
-const filterAndCount = R.pipe(R.filter, R.length)
-const getLetters = R.useWith(filterAndCount, [valuesInclude, countedByLetters])
+const partOne = R.pipe(
+  R.applySpec({
+    twoLetters: getLetters(2),
+    threeLetters: getLetters(3)
+  }),
+  R.converge(R.multiply, [R.prop('twoLetters'), R.prop('threeLetters')]))
+
+const pairWithOneLetterMismatch = ([a, b]) => {
+  const zippedPairs = R.zip(splitIntoCharacters(a), splitIntoCharacters(b))
+  const equalZippedPairs = R.filter(arrayPairIsEqual, zippedPairs)
+
+  // One letter mismatch
+  return equalZippedPairs.length === a.length - 1
+}
+
+const partTwo = R.pipe(
+  createHalfTable,
+  R.find(pairWithOneLetterMismatch),
+  R.converge(R.zip, [R.head, R.last]),
+  R.filter(arrayPairIsEqual),
+  R.map(R.last),
+  R.join('')
+)
+
