0612a7780454b6ce550eca08d9d6c6fc682db7d8
Alexander Lichter <manniL@gmx.net>
2018-12-11 10:59:50 +0000

    More code improvements

--- 03/index.js
+++ 03/index.js
@@ -2,27 +2,24 @@ const R = require('ramda')
 const {readFileAndSplitByLines} = require('../utils/fs.js')
 const path = require('path')
 
-const partOne = input => {
-  const inches = R.reduce(getInchesForFabric, {current: new Map(), dupes: new Map()}, input)
-  // count of all elements - count of unique elements = count of duplicate items
-  return inches.dupes.size
-}
-
 const partTwo = input => {
   const ids = R.pluck('id', input)
   const inches = R.reduce(getInchesWithDupesForFabric, {current: new Map(), dupes: new Map()}, input)
-  // count of all elements - count of unique elements = count of duplicate items
+
   const seenIds = new Set()
   const addToSeen = a => a.forEach(v => seenIds.add(v))
   inches.dupes.forEach(addToSeen)
-  return R.difference(ids, Array.from(seenIds))[0]
+  return R.pipe(
+    R.difference(ids),
+    R.head
+  )(Array.from(seenIds))
 }
 
 const getInchesWithDupesForFabric = (({current, dupes}, {id, position, size}) => {
   // "Zero based"
   const rangeX = R.range(position.x + 1, position.x + size.x + 1)
   const rangeY = R.range(position.y + 1, position.y + size.y + 1)
-  const pairs = R.map(([x, y]) => `${x}_${y}`, R.xprod(rangeY, rangeX))
+  const pairs = R.map(R.join('_'), R.xprod(rangeY, rangeX))
   pairs.forEach(pair => {
     if (dupes.has(pair)) {
       dupes.set(pair, dupes.get(pair).concat(id))
@@ -43,7 +40,7 @@ const getInchesForFabric = (({current, dupes}, {id, position, size}) => {
   // "Zero based"
   const rangeX = R.range(position.x + 1, position.x + size.x + 1)
   const rangeY = R.range(position.y + 1, position.y + size.y + 1)
-  const pairs = R.map(([x, y]) => `${x}_${y}`, R.xprod(rangeY, rangeX))
+  const pairs = R.map(R.join('_'), R.xprod(rangeY, rangeX))
   pairs.forEach(pair => {
     if (dupes.has(pair)) {
       dupes.set(pair, dupes.get(pair).concat(id))
@@ -77,6 +74,12 @@ const formatInput = R.map(str => {
 })
 
 const formattedInput = formatInput(input)
+
+const partOne = R.pipe(
+  R.reduce(getInchesForFabric, {current: new Map(), dupes: new Map()}),
+  R.prop('dupes'),
+  dupes => dupes.size
+)
 
 console.log('Part 1:', partOne(formattedInput))
 console.log('Part 2:', partTwo(formattedInput))
